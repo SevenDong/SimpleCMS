efbd27fa1f90ee8b82f537c88fd4c26576b091ef
dsw <dsw@DESKTOP-K67KBPO>
2019-07-22 16:54:10 +0800

    配置identity

--- SimpleCMS/Global.asax.cs
+++ SimpleCMS/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SimpleCMS.Migrations;
+using SimpleCMS.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +16,7 @@ namespace SimpleCMS
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
     }
 }
